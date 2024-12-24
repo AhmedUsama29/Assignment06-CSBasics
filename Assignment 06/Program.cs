39b1d7b4945c84702c86605918eef34285e3ad82
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2024-12-24 16:24:06 +0200

    Q2

--- Assignment 06/Program.cs
+++ Assignment 06/Program.cs
@@ -18,20 +18,31 @@
 
             //Example
 
-            int[] numbers = { 1, 2, 3 };
+            //int[] numbers = { 1, 2, 3 };
 
-            //by value
-            ModifyByValue(numbers);
-            Console.WriteLine("After ModifyByValue: " + string.Join(", ", numbers)); // 10, 2, 3
+            ////by value
+            //ModifyByValue(numbers);
+            //Console.WriteLine("After ModifyByValue: " + string.Join(", ", numbers)); // 10, 2, 3
 
-            //by reference
-            ModifyByReference(ref numbers);
-            Console.WriteLine("After ModifyByReference: " + string.Join(", ", numbers)); // 100, 200, 300
+            ////by reference
+            //ModifyByReference(ref numbers);
+            //Console.WriteLine("After ModifyByReference: " + string.Join(", ", numbers)); // 100, 200, 300
 
 
             #endregion
-        
-        
+
+            #region Q2: Write a c# Function that accept 4 parameters from user and return result of summation and subtracting of two numbers
+
+            //int sum, mul;
+            //SumMul(10, 20, out sum, out mul);
+            //Console.WriteLine("Sum: " + sum); // 30
+            //Console.WriteLine("Mul: " + mul); // 200
+
+            #endregion
+
+            #region Q3: 
+
+            #endregion
         }
 
         #region Q1 Methods
@@ -47,6 +58,16 @@
         {
 
             arr = new int[] { 100, 200, 300 };
+        }
+        #endregion
+
+        #region Q2 Methods
+        static void SumMul(int num1, int num2, out int sum, out int mul)
+        {
+
+            sum = num1 + num2;
+            mul = num1 * num2;
+
         } 
         #endregion
 
